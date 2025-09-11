e51caa775876b160aa20a837241cbd5a83651dc9
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-09-11 12:37:13 +0700

    BE: add Page findAllUser in Repo

--- server/src/main/java/com/FastFoodDelivery/repository/UserRepository.java
+++ server/src/main/java/com/FastFoodDelivery/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.FastFoodDelivery.repository;
 
 import com.FastFoodDelivery.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+    Page<User> findAllUser(Pageable pageable);
 }
