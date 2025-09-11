4a6c2321214bc79dc5873d87941a4fdf2db67bd3
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-09-11 07:28:33 +0700

    BE: create project

---
+++ server/src/main/java/com/FastFoodDelivery/FastFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.FastFoodDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodDeliveryApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/FastFoodDelivery/FastFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FastFoodDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
