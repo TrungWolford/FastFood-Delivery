cafd113c2e0343cda892f5eca0f8ab940eb51446
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-09-11 08:55:25 +0700

    BE: create User repo

---
+++ server/src/main/java/com/FastFoodDelivery/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.FastFoodDelivery.repository;
+
+import com.FastFoodDelivery.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
