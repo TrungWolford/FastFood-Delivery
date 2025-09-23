afdf454d45ef8957c82ee9f98cdc5c306e89de6c
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-09-23 12:10:01 +0700

    BE: Update securityConfig

--- server/src/main/java/com/FastFoodDelivery/config/SecurityConfig.java
+++ server/src/main/java/com/FastFoodDelivery/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.FastFoodDelivery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,12 +11,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(basic -> {});
+                        .anyRequest().permitAll() // Cho phép tất cả requests mà không cần authentication
+                );
         return http.build();
     }
 }
