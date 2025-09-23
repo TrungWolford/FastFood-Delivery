d51401c67b9f0602642fbe61308641ed2f358ab2
hualapthanh <hualapthanh@gmail.com>
2025-09-23 21:02:27 +0700

    BE: Create Entity Cart

---
+++ server/src/main/java/com/FastFoodDelivery/entity/Cart.java
@@ -0,0 +1,23 @@
+package com.FastFoodDelivery.entity;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Document(collection = "carts")
+@Data
+public class Cart {
+    @Id
+    private ObjectId cartId;
+    private Object userId;
+    private Object restaurantId;
+    private List<CartItem> cartItems;
+    private Date createdAt;
+    private Date updatedAt;
+}
