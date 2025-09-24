14e44d63622c0b7165fcf73beb623dd8b0c2dbfb
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-09-24 17:37:28 +0700

    BE: create RestaurantController

---
+++ server/src/main/java/com/FastFoodDelivery/controller/RestaurantController.java
@@ -0,0 +1,66 @@
+﻿package com.FastFoodDelivery.controller;
+
+import com.FastFoodDelivery.dto.request.Restaurant.CreateRestaurantRequest;
+import com.FastFoodDelivery.dto.request.Restaurant.UpdateRestaurantRequest;
+import com.FastFoodDelivery.dto.response.Restaurant.RestaurantResponse;
+import com.FastFoodDelivery.entity.Restaurant;
+import com.FastFoodDelivery.service.RestaurantService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/restaurants")
+public class RestaurantController {
+    
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @GetMapping
+    public ResponseEntity<Page<RestaurantResponse>> getAllRestaurants(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(restaurantService.getAllRestaurants(pageable));
+    }
+
+    @GetMapping("/{restaurantId}")
+    public ResponseEntity<RestaurantResponse> getRestaurantById(@PathVariable String restaurantId) {
+        RestaurantResponse restaurant = restaurantService.getRestaurantById(restaurantId);
+        return ResponseEntity.ok(restaurant);
+    }
+
+    @GetMapping("/owner/{ownerId}")
+    public ResponseEntity<List<RestaurantResponse>> getRestaurantsByOwnerId(@PathVariable String ownerId) {
+        ObjectId objectId = new ObjectId(ownerId);
+        List<RestaurantResponse> restaurants = restaurantService.getRestaurantsByOwnerId(objectId);
+        return ResponseEntity.ok(restaurants);
+    }
+
+    @PostMapping
+    public ResponseEntity<RestaurantResponse> createRestaurant(@RequestBody @Valid CreateRestaurantRequest request) {
+        RestaurantResponse createdRestaurant = restaurantService.createRestaurant(request);
+        return ResponseEntity.ok(createdRestaurant);
+    }
+
+    @PutMapping("/{restaurantId}")
+    public ResponseEntity<RestaurantResponse> updateRestaurant(@RequestBody UpdateRestaurantRequest request,
+                                                      @PathVariable String restaurantId) {
+        RestaurantResponse updatedRestaurant = restaurantService.updateRestaurant(request, restaurantId);
+        return ResponseEntity.ok(updatedRestaurant);
+    }
+
+    @DeleteMapping("/{restaurantId}")
+    public ResponseEntity<String> deleteRestaurant(@PathVariable String restaurantId) {
+        restaurantService.deleteRestaurant(restaurantId);
+        return ResponseEntity.ok("Restaurant deleted successfully");
+    }
+}
