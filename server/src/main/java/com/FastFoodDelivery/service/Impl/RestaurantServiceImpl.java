424a6fe77aeb7e31e52f8b04c03dfa1b0bc29be0
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-09-24 17:37:07 +0700

    BE: create Restaurant service

---
+++ server/src/main/java/com/FastFoodDelivery/service/Impl/RestaurantServiceImpl.java
@@ -0,0 +1,93 @@
+﻿package com.FastFoodDelivery.service.Impl;
+
+import com.FastFoodDelivery.dto.request.Restaurant.CreateRestaurantRequest;
+import com.FastFoodDelivery.dto.request.Restaurant.UpdateRestaurantRequest;
+import com.FastFoodDelivery.dto.response.Restaurant.RestaurantResponse;
+import com.FastFoodDelivery.dto.response.User.UserResponse;
+import com.FastFoodDelivery.entity.Restaurant;
+import com.FastFoodDelivery.entity.User;
+import com.FastFoodDelivery.exception.ResourceNotFoundException;
+import com.FastFoodDelivery.repository.RestaurantRepository;
+import com.FastFoodDelivery.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.bson.types.ObjectId;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RestaurantServiceImpl implements RestaurantService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @Override
+    public List<RestaurantResponse> getAllRestaurants() {
+
+        return restaurantRepository.findAll().stream()
+                .map(RestaurantResponse::fromEntity)
+                .toList();
+    }
+
+    @Override
+    public Page<RestaurantResponse> getAllRestaurants(Pageable pageable) {
+        return restaurantRepository.findAll(pageable)
+                .map(RestaurantResponse::fromEntity);
+    }
+
+    @Override
+    public RestaurantResponse getRestaurantById(String restaurantId) {
+        Restaurant restaurant =  restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new ResourceNotFoundException("Restaurant", "id", restaurantId));
+
+        return RestaurantResponse.fromEntity(restaurant);
+    }
+
+    @Override
+    public List<RestaurantResponse> getRestaurantsByOwnerId(ObjectId ownerId) {
+        return restaurantRepository.findByOwnerId(ownerId)
+                .stream().map(RestaurantResponse::fromEntity).toList();
+    }
+
+    @Override
+    public RestaurantResponse createRestaurant(CreateRestaurantRequest request) {
+        Restaurant restaurant = new Restaurant();
+        restaurant.setOwnerId(request.getOwnerId());
+        restaurant.setRestaurantName(request.getRestaurantName());
+        restaurant.setAddress(request.getAddress());
+        restaurant.setPhone(request.getPhone());
+        restaurant.setOpeningHours(request.getOpeningHours());
+        restaurant.setDescription(request.getDescription());
+        restaurant.setCreatedAt(new Date());
+        restaurant.setUpdatedAt(new Date());
+
+        restaurantRepository.save(restaurant);
+
+        return RestaurantResponse.fromEntity(restaurant);
+    }
+
+    @Override
+    public RestaurantResponse updateRestaurant(UpdateRestaurantRequest request, String restaurantId) {
+        Restaurant restaurant =  restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new ResourceNotFoundException("Restaurant", "id", restaurantId));
+
+        restaurant.setRestaurantName(request.getRestaurantName());
+        restaurant.setAddress(request.getAddress());
+        restaurant.setPhone(request.getPhone());
+        restaurant.setOpeningHours(request.getOpeningHours());
+        restaurant.setDescription(request.getDescription());
+        restaurant.setUpdatedAt(new Date());
+        restaurantRepository.save(restaurant);
+
+        return RestaurantResponse.fromEntity(restaurant);
+    }
+
+    @Override
+    public void deleteRestaurant(String restaurantId) {
+        restaurantRepository.deleteById(restaurantId);
+    }
+}
---
+++ server/src/main/java/com/FastFoodDelivery/service/RestaurantService.java
@@ -0,0 +1,22 @@
+﻿package com.FastFoodDelivery.service;
+
+import com.FastFoodDelivery.dto.request.Restaurant.CreateRestaurantRequest;
+import com.FastFoodDelivery.dto.request.Restaurant.UpdateRestaurantRequest;
+import com.FastFoodDelivery.dto.response.Restaurant.RestaurantResponse;
+import com.FastFoodDelivery.entity.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RestaurantService {
+    List<RestaurantResponse> getAllRestaurants();
+    Page<RestaurantResponse> getAllRestaurants(Pageable pageable);
+    RestaurantResponse getRestaurantById(String restaurantId);
+    List<RestaurantResponse> getRestaurantsByOwnerId(ObjectId ownerId);
+    RestaurantResponse createRestaurant(CreateRestaurantRequest request);
+    RestaurantResponse updateRestaurant(UpdateRestaurantRequest request, String restaurantId);
+    void deleteRestaurant(String restaurantId);
+}
