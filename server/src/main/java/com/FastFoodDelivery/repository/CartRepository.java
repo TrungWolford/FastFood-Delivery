ff7c3f82e1e2576e6e6544d385f0219529b7cd89
hualapthanh <hualapthanh@gmail.com>
2025-09-24 21:28:07 +0700

    BE: Create Cart Repository

---
+++ server/src/main/java/com/FastFoodDelivery/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.FastFoodDelivery.repository;
+
+import com.FastFoodDelivery.entity.Cart;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends MongoRepository<Cart, String> {
+}
