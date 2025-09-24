ee4a7535f78a9e7428062d2768d7066a4c47f9e5
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-09-24 16:52:32 +0700

    BE: create RestaurantRepository

---
+++ server/src/main/java/com/FastFoodDelivery/repository/RestaurantRepository.java
@@ -0,0 +1,13 @@
+﻿package com.FastFoodDelivery.repository;
+
+import com.FastFoodDelivery.entity.Restaurant;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+@Repository
+public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
+    List<Restaurant> findByOwnerId(ObjectId ownerId);
+}
