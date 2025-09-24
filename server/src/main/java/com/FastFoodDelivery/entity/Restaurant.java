17fe51c1843b8030c200c08bb57f152e2d8e3e93
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-09-24 11:02:50 +0700

    BE: create entity Restaurant

---
+++ server/src/main/java/com/FastFoodDelivery/entity/Restaurant.java
@@ -0,0 +1,25 @@
+﻿package com.FastFoodDelivery.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.bson.types.ObjectId;
+
+import java.util.Date;
+
+@Document(collection = "restaurants")
+@Data
+public class Restaurant {
+    @Id
+    private String restaurantId;
+    private ObjectId ownerId;
+    private String restaurantName;
+    private String address;
+    private String phone;
+    private String openingHours;
+    private double rating;
+    private Date createdAt;
+    private int status;           // New field
+    private Date updatedAt;       // New field  
+    private String description;   // New field
+}
