7fab13c7ac56bb1e34a3431e894324c2a5c5445b
hualapthanh <hualapthanh@gmail.com>
2025-09-25 17:46:19 +0700

    BE: Create CartRepository

--- server/src/main/java/com/FastFoodDelivery/repository/CartRepository.java
+++ server/src/main/java/com/FastFoodDelivery/repository/CartRepository.java
@@ -2,9 +2,15 @@ package com.FastFoodDelivery.repository;
 
 import com.FastFoodDelivery.entity.Cart;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends MongoRepository<Cart, ObjectId> {
+    Optional<Cart> findByCartId(ObjectId cartId);
+    Page<Cart> findByUserId(ObjectId userId, Pageable pageable);
 }
