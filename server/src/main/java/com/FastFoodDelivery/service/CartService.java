d2960bd68a633073ba0bdf292a21e80ed49e4985
hualapthanh <hualapthanh@gmail.com>
2025-09-25 17:47:08 +0700

    BE: Create CartService

--- server/src/main/java/com/FastFoodDelivery/service/CartService.java
+++ server/src/main/java/com/FastFoodDelivery/service/CartService.java
@@ -1,4 +1,19 @@
 package com.FastFoodDelivery.service;
 
+import com.FastFoodDelivery.dto.request.Cart.CreateCartRequest;
+import com.FastFoodDelivery.dto.request.Cart.UpdateCartRequest;
+import com.FastFoodDelivery.dto.response.Cart.CartResponse;
+import com.FastFoodDelivery.dto.response.CartItem.CartItemResponse;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
 public interface CartService {
+    CartResponse getCartById(ObjectId cartId);
+    List<CartResponse> getCartsByUserId(Object userId);
+    List<CartItemResponse> getCartItemsByCartId(ObjectId cartId);
+
+    CartResponse createCart(CreateCartRequest request);
+    CartResponse updateCart(UpdateCartRequest request, Object cartId);
+    void deleteCart(ObjectId cartId);
 }
