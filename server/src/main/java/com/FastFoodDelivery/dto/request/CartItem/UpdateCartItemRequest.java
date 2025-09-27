0dfa566232422a70ed8ec0e072d02b5463d10415
hualapthanh <hualapthanh@gmail.com>
2025-09-27 15:53:06 +0700

    BE: Update UpdateCartItemRequest add cartItemId

--- server/src/main/java/com/FastFoodDelivery/dto/request/CartItem/UpdateCartItemRequest.java
+++ server/src/main/java/com/FastFoodDelivery/dto/request/CartItem/UpdateCartItemRequest.java
@@ -1,9 +1,11 @@
 package com.FastFoodDelivery.dto.request.CartItem;
 
 import lombok.Data;
+import org.bson.types.ObjectId;
 
 @Data
 public class UpdateCartItemRequest {
+    private ObjectId cartItemId;
     private Integer quantity;
     private String note;
 }
