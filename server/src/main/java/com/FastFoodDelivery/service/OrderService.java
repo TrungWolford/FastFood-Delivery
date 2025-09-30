afd1fe0b07701bab04522c8e58dc989e4fe9672f
hualapthanh <hualapthanh@gmail.com>
2025-09-30 17:08:28 +0700

    BE: Add OrderService

--- server/src/main/java/com/FastFoodDelivery/service/OrderService.java
+++ server/src/main/java/com/FastFoodDelivery/service/OrderService.java
@@ -1,4 +1,22 @@
 package com.FastFoodDelivery.service;
 
+import com.FastFoodDelivery.dto.request.Order.CreateOrderRequest;
+import com.FastFoodDelivery.dto.request.Order.UpdateOrderRequest;
+import com.FastFoodDelivery.dto.response.Order.OrderResponse;
+import com.FastFoodDelivery.dto.response.OrderItem.OrderItemResponse;
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface OrderService {
+    OrderResponse getOrderById(ObjectId orderId);
+    Page<OrderResponse> getOrdersByCustomerId(ObjectId customerId, Pageable pageable);
+    Page<OrderResponse> getOrdersByRestaurantId(ObjectId restaurantId, Pageable pageable);
+    List<OrderItemResponse> getOrderItemsByOrderId(ObjectId orderId);
+
+    OrderResponse createOrder(CreateOrderRequest request);
+    OrderResponse updateOrder(UpdateOrderRequest request, ObjectId orderId);
+    OrderResponse cancelOrder(ObjectId orderId);
 }
