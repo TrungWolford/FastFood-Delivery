1295d3babadba2e39a18db0e526013048049565c
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-10-17 19:11:01 +0700

    BE: Create DeliveryController

---
+++ server/src/main/java/com/FastFoodDelivery/controller/DeliveryController.java
@@ -0,0 +1,47 @@
+package com.FastFoodDelivery.controller;
+
+import com.FastFoodDelivery.dto.request.Delivery.CreateDeliveryRequest;
+import com.FastFoodDelivery.dto.request.Delivery.UpdateDeliveryRequest;
+import com.FastFoodDelivery.dto.request.Delivery.UpdateStatusDeliveryRequest;
+import com.FastFoodDelivery.dto.response.Delivery.DeliveryResponse;
+import com.FastFoodDelivery.service.DeliveryService;
+import jakarta.validation.Valid;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/deliveries")
+public class DeliveryController {
+    @Autowired
+    private DeliveryService deliveryService;
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<List<DeliveryResponse>> getALlDeliveriesByOrderId(@PathVariable ObjectId orderId){
+        return ResponseEntity.ok(deliveryService.getALlDeliveriesByOrderId(orderId));
+    }
+
+    @GetMapping("/{deliveryId}")
+    public ResponseEntity<DeliveryResponse> getByDeliveryId(@PathVariable ObjectId deliveryId){
+        return ResponseEntity.ok(deliveryService.getByDeliveryId(deliveryId));
+    }
+
+    @PostMapping
+    public ResponseEntity<DeliveryResponse> createDelivery(@Valid @RequestBody CreateDeliveryRequest request){
+        return ResponseEntity.ok(deliveryService.createDelivery(request));
+    }
+
+    @PutMapping("/{deliveryId}")
+    public ResponseEntity<DeliveryResponse> getByDeliveryId(@Valid @RequestBody UpdateDeliveryRequest request, @PathVariable ObjectId deliveryId){
+        return ResponseEntity.ok(deliveryService.updateDelivery(request, deliveryId));
+    }
+
+    @PatchMapping("/{deliveryId}")
+    public ResponseEntity<String> changeStatus(@Valid @RequestBody UpdateStatusDeliveryRequest request, @PathVariable ObjectId deliveryId){
+        deliveryService.changeStatus(request, deliveryId);
+        return ResponseEntity.ok("Change status successfully");
+    }
+}
