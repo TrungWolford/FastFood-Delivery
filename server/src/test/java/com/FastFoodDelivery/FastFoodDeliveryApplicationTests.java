9ee4c2981e1ffbce005a4c25ba4bb6c4c2c954ce
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-11-07 15:10:45 +0700

    BE: add test config

--- server/src/test/java/com/FastFoodDelivery/FastFoodDeliveryApplicationTests.java
+++ server/src/test/java/com/FastFoodDelivery/FastFoodDeliveryApplicationTests.java
@@ -2,12 +2,18 @@ package com.FastFoodDelivery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+	"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration," +
+		"org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration"
+})
 class FastFoodDeliveryApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test verifies that the Spring application context loads successfully
 	}
 
 }
