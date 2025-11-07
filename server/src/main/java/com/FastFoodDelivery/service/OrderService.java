b34d2aa364009434675e5f22499c086b547ac000
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-11-07 23:08:48 +0700

    BE: Create getAllOrder

--- server/src/main/java/com/FastFoodDelivery/service/OrderService.java
+++ server/src/main/java/com/FastFoodDelivery/service/OrderService.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public interface OrderService {
     OrderResponse getOrderById(ObjectId orderId);
+    Page<OrderResponse> getAllOrders(Pageable pageable);
     Page<OrderResponse> getOrdersByCustomerId(ObjectId customerId, Pageable pageable);
     Page<OrderResponse> getOrdersByRestaurantId(ObjectId restaurantId, Pageable pageable);
     List<OrderItemResponse> getOrderItemsByOrderId(ObjectId orderId);
