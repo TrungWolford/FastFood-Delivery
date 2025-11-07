edc664a5515e32b6c08a79e3501aab93c0ec363a
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-11-07 17:33:15 +0700

    BE: fix test

--- server/src/test/java/com/FastFoodDelivery/FastFoodDeliveryApplicationTests.java
+++ server/src/test/java/com/FastFoodDelivery/FastFoodDeliveryApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-	"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration," +
-		"org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration"
+	"spring.autoconfigure.exclude=" +
+		"org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration," +
+		"org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration," +
+		"org.springframework.boot.autoconfigure.data.mongo.MongoAutoConfiguration," +
+		"org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration"
 })
 class FastFoodDeliveryApplicationTests {
 
